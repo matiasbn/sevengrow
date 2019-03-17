732a338d2626246f12b811ccdeb987942ea83e11
Matias <matias.barriosn@gmail.com>
2019-03-17 16:42:13 -0300

    testing heroku env

--- redis.js
+++ redis.js
@@ -2,10 +2,6 @@ const redis = require('redis');
 
 var client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_URL);
 
-client.auth(process.env.REDIS_PASSWORD, function (err, res) {
-    if (err) {
-        response.end("err: " + err);
-    }
-});
+client.auth(process.env.REDIS_PASSWORD);
 
 module.exports = client;
